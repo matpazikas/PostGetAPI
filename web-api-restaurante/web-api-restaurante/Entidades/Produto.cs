9dccf5efecf735a6951635cc3ebb3c7ff7bc2740
matpazikas <matheuspazikas@gmail.com>
2024-05-16 22:35:20 -0300

    fim da aula

---
+++ web-api-restaurante/web-api-restaurante/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace web_api_restaurante.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set;}
+        public string Descricao { get; set;}
+        public string ImagemUrl { get; set;}
+
+    }
+}
